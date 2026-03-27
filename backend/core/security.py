b47c81d35d2b7cbb7ac57e0d36246f2ca432c62c
yuukinishida <ynishida08@gmail.com>
2026-03-26 22:25:46 -0400

    Set up core folder and main.py

---
+++ backend/core/security.py
@@ -0,0 +1,53 @@
+from typing import Annotated
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from pwdlib import PasswordHash
+from sqlalchemy.orm import Session
+from datetime import datetime, timedelta, timezone
+
+import jwt
+from jwt.exceptions import InvalidTokenError
+
+from core.config import settings
+from schemas.auth import TokenData
+from models.users import User as UserModel
+from db.database import get_db
+
+REFRESH_TOKEN_EXPIRE_MINUTES = 6 * 24 * 7
+
+password_hash = PasswordHash.recommended()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_db)) -> UserModel:
+    token_data = decode_token(token)
+    user = db.query(UserModel).filter(UserModel.email == token_data.email).first()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+    return user
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+def decode_token(token: str) -> TokenData:
+    credentials_exceptions = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        email: str = payload.get("sub")
+        if email is None:
+            raise credentials_exceptions
+        return TokenData(email=email)
+    except InvalidTokenError:
+        raise credentials_exceptions
+
+def get_hashed_password(password: str):
+    return password_hash.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str):
+    return password_hash.verify(plain_password, hashed_password)
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI(
+    title="DishCraft",
+    description="api to craft, store, manage recipes generated or manually added",
+    version="0.1.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+)
+
+# CREATE DATABASE TABLES
+
+# TODO: ADD APP ROUTES HERE
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
